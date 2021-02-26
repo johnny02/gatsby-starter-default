b318dea341f11486f9b3ed697613959cd73ad969
johnny o2 <tomaileinai@yahoo.gr>
2021-02-26 18:16:59 +0200

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,2 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
 
-    <Link to="/page-2/">Go to page 2</Link> <br />
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
